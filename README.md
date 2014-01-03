474210e65b8e17340a34da8c12cf526f4797b097
Benjamin Mosnier <bm@kappuccino.org>
2014-01-03 18:03:43 +0100

    Init
    Signed-off-by: Benjamin Mosnier <bm@kappuccino.org>

---
+++ README.md
@@ -0,0 +1,125 @@
+# Ben
+---
+
+Ben is an expressejs middleware to create picture thumbnails.
+
+## Install
+`npm install` installs imagemagick **wrapper**, so you need to install imagemagick binaries too.
+
+###### OSX
+Brew make things really simple  
+
+	# brew update
+	# brew install imagemagick
+	... wait your beer
+
+###### Linux
+You can [build from source](http://www.imagemagick.org/script/binary-releases.php) or ask [google](https://www.google.fr/search?q=imagemagick+debian+ubuntu).
+
+
+## Usage
+This is a very basic bootstrap
+	
+	var express = require('express')
+		, ben = require('ben')
+		, app = express()
+
+	app.configure(function(){
+		app.set('port', process.env.PORT || 3000);
+		app.use(express.logger('dev'));
+		app.use(ben({
+			// check options bellow
+		}));
+	});
+	
+	http.createServer(app).listen(app.get('port'), function () {
+		console.log("Express server listening on port " + app.get('port'));
+	});
+
+#### Options
+
+###### — root (required)
+the folder containing source pictures (absolute path)  
+
+###### — cache (required)
+this folder stores cache data,  if this folder does not exists, it will be created with the first generation
+
+###### — ttl (default 1 day)
+max-age header sent to brower in seconds.
+
+###### — cacheTTL (default 1 day)
+time to serve cached version in seconds. To totally disable cache, use 0.
+
+###### — allowedExtensions (default ['gif', 'png', 'jpg'])
+array of extension allowed. Used in the regexp to check if the request could be handled.
+
+###### — quality (default 80)
+jpeg files compression factor, from 1 (light but ugly) to 100 (max but heavy) This parameter is overwriten if `q:` url parameter is used. This could be used to define a global default quality value for all request.
+
+
+### Scheme
+
+Image generation are request url based. All you need is to call this URL: 
+
+	http://localhost:3000/key:val[,key2:val2]/path/to/file.jpg
+
+You can add more keys. Ex: 300px height and a very poor quality: 
+
+	http://localhost:3000/h:300,q:10/path/to/file.jpg
+
+#### Keys
+
+###### — h (=height)
+Define the Height of the picture, respect the ratio
+
+###### — w (=width)
+Same behaviour as `h` but set the width
+
+###### — s (=square)
+Create a square picture. This method uses the `cut`and `gravity` parameters from imagemagick
+
+###### — c (=crop)
+Crop the picture with `w` and `h` parameters. (c:800.100) for a 800px width and 100px height. By default the crop is centered.  
+You can specify a region with `g:value` option. Allowed values are: [NorthWest, North, NorthEast, West, Center, East, SouthWest, South, SouthEast] (case sensitive)
+
+###### — q (=qality)
+Specify a quality between 0 to 100. Default options is used (80)
+
+**NOTE**
+
+- `h:100,w:100` is equivalent to `s:100`. The square mode is shorter.
+
+- `h:100,w:800` is equivalent to `c:800.100`. The crop mode is shorter.
+
+- If you set a value bigger than the source picture is, the source value is kept. For example, if you have 800px width picture and you ask for a 999px picture width, the generated will be 800px width.  
+
+### Errors
+
+If scheme is not respected, a 500 error is produced, with a gently error message dispayed in the browser.  
+
+
+### Examples
+
+http://localhost:3000/`w:400`/file.jpg => force Width  
+
+http://localhost:3000/`h:500`/file.jpg => force Height  
+
+http://localhost:3000/`s:600`/file.jpg => square image  
+
+http://localhost:3000/`c:600.100`/file.jpg => panoramic picture
+
+http://localhost:3000/`c:600.100:g:South,q:10`/file.jpg => panoramic picture cropped to South region and a vey low quality
+
+
+### Tips
+
+If you want to know if the image delivered is cached or not, have a look to `X-Hit-Cache` header, (1 = cached, 0 = not cached)
+
+
+
+  
+    
+    
+    
+     
+
---
+++ index.js
@@ -0,0 +1,279 @@
+var mkdirp    = require('mkdirp')
+	, moment    = require('moment')
+	, request   = require('request')
+	, send      = require('send')
+	, im        = require('imagemagick')
+	, path      = require('path')
+	, fs        = require('fs')
+	, crypto    = require('crypto')
+
+var options = {}
+	, regexp = ''
+	, rootDir
+	, cacheDir
+	, ttl
+	, cacheTTL
+	, quality;
+
+
+// -- Constructor -------------------------------------------------------------
+
+var media = function(opts) {
+
+	_parseOptions(opts || {})
+
+	return function (req, res, next) {
+
+		function resume(runNext){
+			if (runNext) next();
+		}
+
+		// Only GET + HEAD, if not NEXT()
+		if (req.method != 'GET' && req.method != 'HEAD') return resume(true);
+
+		// If the URL is not valid (regexp matching) NEXT()
+		var request = req.originalUrl.match(regexp);
+		if (!request) return resume(true);
+
+		//----------------------------------------------------------
+
+		var keyVal = new RegExp(':')
+			, params = request[1].split(/,/)  // h:400,q:80
+			, url = rootDir + request[2]      // /var/www/public/toto/truc/monimage.jpg
+			, parameters = {}                 // {h: 400, q: 80}
+
+		if(params.length){
+			params.forEach(function(e, i){
+				var kv = e.split(keyVal)
+				parameters[kv[0]] = kv[1]
+			})
+		}else{
+			res.writeHead(500)
+			res.end('No parameter')
+			return resume(false)
+		}
+
+		if(!fs.existsSync(url)){
+			res.writeHead(404)
+			res.end('Not found')
+			return resume(false)
+		}
+
+		//----------------------------------------------------------
+
+		var cachedFile = _destination(url, parameters)
+
+		// see if we can serve the file from file cache, if ttl has not yet expired
+		if (cacheTTL > 0) {
+			try {
+				var stats = fs.statSync(cachedFile)
+					, fileUnix = moment(stats.mtime).unix()
+					, nowUnix = moment().unix()
+					, diffUnix = nowUnix - fileUnix;
+
+				// file is fresh, no need to download/resize etc.
+				if (diffUnix < cacheTTL) {
+					res.setHeader('X-Hit-Cache', '1');
+					send(req, cachedFile).maxage(ttl || 0).pipe(res);
+					return resume(false);
+				}
+			} catch (err) {
+				// no action necessary, just continue with normal flow
+			}
+		}
+
+		parameters.src = url;
+		parameters.dst = cachedFile;
+
+		_generate(parameters, function(err) {
+				if (err){
+					if(typeof err == 'string'){
+						res.writeHead(500)
+						res.end(err);
+						return resume(false)
+					}else{
+						throw err;
+					}
+				}
+
+				res.setHeader('X-Hit-Cache', '0');
+				send(req, cachedFile).maxage(ttl || 0).pipe(res);
+
+				return resume(false);
+			}
+		);
+	}
+
+}
+
+
+// -- Internal Jam ------------------------------------------------------------
+
+var _parseOptions = function (options) {
+
+	var day  = 3600 * 24;
+
+	ttl      = ('ttl' in options) ? parseInt(options.ttl) : day;  // max-age for 1 day by default.
+	cacheTTL = ('cacheTTL' in options) ? options.cacheTTL : day;  // use local cache for 1 day by default
+	quality  = ('quality' in options) ? parseInt(options.quality) / 100 : 0.8;
+
+//	console.log('ttl='+ttl, 'cacheTTL='+cacheTTL);
+
+	rootDir  = options.root;
+	cacheDir = options.cache || __dirname + '/cache';
+
+	if(!'root' in options || options.root == '' || !fs.existsSync(options.root)){
+		throw new Error('root parameter is not defined, empty or not founds');
+	}
+
+	if(!'cache' in options || options.cache == '' || !fs.existsSync(options.cache)){
+		throw new Error('cache parameter is not defined, empty or not found');
+	}
+
+	var allowedExtensions = options.allowedExtensions || ['gif', 'png', 'jpg'];
+	for (i=0; i < allowedExtensions.length; i++) {
+		if (allowedExtensions[i][0] === '.') allowedExtensions[i] = allowedExtensions[i].substring(1);
+	}
+
+	regexp = new RegExp(
+		'^/' + '([a-z]:[^/]*)' + '(/(.*)' + '\.(?:' + allowedExtensions.join('|') + '))$',
+		'i'
+	);
+
+}
+
+var _generate = function (opt, callback) {
+
+	var src = opt.src
+		, dst = opt.dst
+
+	// Trouver les dimensions de l'image source
+	im.identify(['-format', '%wx%h', src], function(err, dimension){
+
+		if (err) callback(err);
+
+		var arrDimension = dimension.split("x")
+			, origWidth = parseInt(arrDimension[0])
+			, origHeight = parseInt(arrDimension[1])
+			, gravity =  ['NorthWest', 'North', 'NorthEast', 'West', 'Center', 'East', 'SouthWest', 'South', 'SouthEast']
+			, imOptions = {
+				srcPath: src,
+				dstPath: dst,
+				quality: quality
+			}
+
+		// -- Quality
+		if('q' in opt) imOptions.quality = parseInt(opt.q) / 100;
+
+		// -- H+W=C
+		if('h' in opt && 'w' in opt){
+			opt.c = opt.w+'.'+ opt.h
+			delete opt.w
+			delete opt.h
+		}
+
+		// -- SQUARE
+		if('s' in opt){
+
+			var value = parseInt(opt.s)
+
+			if(origHeight > origWidth && value > origWidth){
+				value = origWidth
+			}else
+			if(origWidth > origHeight && value > origHeight){
+				value = origHeight
+			}
+
+			imOptions.gravity = 'Center';
+			imOptions.width = value;
+			imOptions.height = value;
+
+			im.crop(imOptions, function(err, stdout, stderr){
+				callback(err);
+			});
+
+		}else
+
+		// -- CROP
+		if('c' in opt){
+
+			if('g' in opt && gravity.indexOf(opt.g) > -1){
+				imOptions.gravity = opt.g;
+			}else{
+				imOptions.gravity = 'Center';
+			}
+
+			var arrProp = opt.c.split('.')
+			if(arrProp.length != 2) return callback('Crop mode accept only 2 values: w.h');
+
+			imOptions.width = parseInt(arrProp[0]);
+			if(isNaN(imOptions.width)) return callback('Width value is not valid');
+
+			imOptions.height = parseInt(arrProp[1])
+			if(isNaN(imOptions.height)) return callback('Height value is not valid');
+
+			im.crop(imOptions, function(err, stdout, stderr){
+				callback(err);
+			});
+
+		}else
+
+		// -- HEIGHT
+		if('h' in opt){
+			imOptions.height = parseInt(opt.h);
+
+			if(isNaN(imOptions.height)) return callback('Parameter "h" is not a number');
+			if(imOptions.height > origHeight) imOptions.height = origHeight
+
+		}else
+
+		// -- WIDTH
+		if('w' in opt){
+			imOptions.width  = parseInt(opt.w);
+
+			if(isNaN(imOptions.width)) return callback('Parameter "w" is not a number');
+			if(imOptions.width > origWidth) imOptions.width = origWidth
+
+		}
+		
+		else{
+			return callback('Sorry, what did you say ? No comprendo amigo !')
+		}
+
+		// Do The Magic
+		im.resize(imOptions, function(err, stdout, stderr){
+			callback(err);
+		});
+
+	});
+
+}
+
+var _destination = function (url, parameters){
+
+	var ext = path.extname(url)             // .jpg
+		, dir = path.dirname(request[2])      // /toto/truc
+		, name = path.basename(url, ext)      // monimage
+		, cachedDir = cacheDir + dir
+		, cachedFile, hash
+
+	mkdirp.sync(cachedDir)
+
+	for(k in parameters){
+		hash += '_'+k + parameters[k];
+	}
+
+	cachedFile = cachedDir + '/' + name + hash + ext;
+
+	return cachedFile;
+}
+
+var _error = function(key, req, res){ // Find a better solution to use req + res
+	res.writeHead(500)
+	res.end('param "'+ key +'" is not valid');
+}
+
+
+// -- Exports -----------------------------------------------------------------
+
+exports = module.exports = media
